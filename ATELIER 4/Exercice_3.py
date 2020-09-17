9140cf043bfa025338ff5ca48f4d9df7564e438c
Frau Axel <axel.frau.tr@gmail.com>
2020-09-17 15:36:32 +0200

    finished exo 3 atelier 4

---
+++ ATELIER 4/Exercice_3.py
@@ -0,0 +1,90 @@
+"""
+Exercice Bilan de l'atelier 3 : Calculs, Comptage, Maximum, Fonction Entières, Structure itératives
+
+file       = "Exercice_1.py"
+author     = "Frau Axel"
+credits    = ["Frau Axel"]
+data       = 17/09/2020
+version    = "1.0"
+"""
+
+def places_lettre(ch:str, mot:str) -> list :
+    """ retourne la ou les places d'un caractère dans une chaine de caractère"""
+    res = []
+    for i in range(len(mot)):
+        if mot[i] == ch :
+            res.append(i)
+    return res 
+# print(places_lettre('b','bonbour'))
+# print(places_lettre('a','bonbour'))
+# print(places_lettre('m','bonbour'))
+
+def outputStr(mot:str, l_pos:list) -> str :
+    """Prend un mot et ajoute à 'str_output' aux emplacements indiqué dans 'l_pos' le '-' par la lettre dans le mot"""
+    str_output = ''
+    i = 0
+    while i < len(mot) :
+        if i in l_pos :
+            str_output += mot[i]
+        else :
+            str_output += '-'
+        i += 1
+    return str_output
+    pass
+
+print(outputStr("Bonjour",[0]))
+
+import random 
+def rungame() :
+    lst =["parisa","londres","madrid","berlin","new-york"]
+    lst_len= len(lst)
+    # iRand = random.randint(1, lst_len)
+    iRand = 1
+    errors_list = ["|______","|/ \ ","| T ","| O ","|---] "]
+    # "|---] "
+    # "| O "
+    # "| T "
+    # "|/ \ "
+    # "|______"
+    word_to_guess = outputStr(lst[iRand], [])
+    print("Voici le mot à trouver : {}".format(word_to_guess))
+    l_pos = []
+    errors = 0
+    error_to_display = []
+    while word_to_guess != lst[iRand] and errors != 5:
+        user_letter = input('Entrer une lettre : ')
+        letter_pos = places_lettre(user_letter, lst[iRand])
+        if letter_pos == [] :
+            print("La lettre entrée ne se trouve pas dans le mot : {}".format(word_to_guess))
+            errors += 1
+        else :
+            for i in letter_pos :
+                l_pos.append(i)
+            word_to_guess = outputStr(lst[iRand], l_pos)
+        print("Voici ce que vous avez jusque là : {}".format(word_to_guess))
+        if errors == 0 :
+            print("Encore 5 erreurs autorisées ! faites attention ;)")
+        else :
+            if(errors == 1) :
+                print("1 erreur : ",errors_list[0])
+            if(errors == 2) :
+                print("2 erreurs : ",errors_list[1])
+                print("1 erreur : ",errors_list[0])
+            if(errors == 3) :
+                print("3 erreurs : ",errors_list[2])
+                print("2 erreurs : ",errors_list[1])
+                print("1 erreurs : ",errors_list[0])
+            if(errors == 4) :
+                print("4 erreurs : ",errors_list[3])
+                print("3 erreurs : ",errors_list[2])
+                print("2 erreurs : ",errors_list[1])
+                print("1 erreur : ",errors_list[0])
+            if(errors == 5) :
+                print("5 erreurs : ",errors_list[4])
+                print("4 erreurs : ",errors_list[3])
+                print("3 erreurs : ",errors_list[2])
+                print("2 erreurs : ",errors_list[1])
+                print("1 erreur : ",errors_list[0])
+                print("C'est PERDU !!!!!!! le mot était : {}".format(lst[iRand]))
+
+rungame()
